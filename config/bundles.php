4eba1593e45bfced0fac109a0074d251d9ed3827
eealvarez <familia_alvarez@hotmail.com>
2025-05-18 17:18:20 -0600

    Instalado asset-mapper, asset, twig-pack, bootstrap, asset-map:compile, webpack-encore-bundle symfony/ux-turbo

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
